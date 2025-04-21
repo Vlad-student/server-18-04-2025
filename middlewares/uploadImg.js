a6c34f05959903c006c75f49fd1c48252bc97d32
Vlad-student <vlad.v.terletskiy@gmail.com>
2025-04-21 19:00:34 +0300

    Upload image

---
+++ middlewares/uploadImg.js
@@ -0,0 +1,31 @@
+const { Error } = require("mongoose");
+const multer = require("multer");
+const CONSTANTS = require("../constants");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, CONSTANTS.UPLOAD_FOLDER);
+  },
+  filename: (req, file, cb) => {
+    const uniqFileName = Date.now() + "-" + file.originalname;
+    cb(null, uniqFileName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /png|jpg|jpeg/;
+  const isRightType = allowedTypes.test(file.mimetype);
+  if (isRightType) {
+    cb(null, true);
+  } else {
+    cb(new Error("wrong mimetype"));
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: CONSTANTS.MAX_UPLOAD_SIZE },
+  fileFilter,
+});
+
+module.exports = upload;
---
+++ models/Sport.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const sportSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 64,
+    },
+    isOlimpic: {
+      type: Boolean,
+      default: true,
+    },
+    image: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const Sport = mongoose.model("Sport", sportSchema);
+
+module.exports = Sport;
